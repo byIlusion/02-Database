3b5e031624af7e5cb0ebe9cac54e34a3a04ecd9c
Ilya <by_Ilusion@mail.ru>
2020-07-11 06:34:26 +0700

    Created first SQL-script

---
+++ 01/Creating_DB.sql
@@ -0,0 +1,6 @@
+# Создаем БД example (с префиксом gb_)
+CREATE DATABASE gb_example;
+# Переключаемся на БД gb_example
+USE gb_example;
+# Создаем таблицу users
+CREATE TABLE users(id INT, name VARCHAR(100));
