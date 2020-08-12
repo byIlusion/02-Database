3b8d6db089bcb95e0481e7b1f5347f0f0e150379
Ilusion <by_Ilusion@mail.ru>
2020-08-12 18:02:47 +0700

    Tasks 09-2 is complete.

--- 09/2 - Administration/tasks_09-2.sql
+++ 09/2 - Administration/tasks_09-2.sql
@@ -9,22 +9,48 @@ use mysql;
 -- Смотрим юзеров
 select Host, User from user;
 
--- Добавляем юзера shop_read
-create user shop_read;
--- Даем право на чтение
-grant select on *.* to shop_read;
+-- Добавляем юзера shop_read с доступом с любого хоста
+create user 'shop_read'@'%';
+-- Даем право только на чтение со всех БД и всех таблиц
+grant select on *.* to 'shop_read'@'%';
 
--- Добавляем юзера shop
-create user shop@localhost identified with mysql_native_password by '1234';
--- Даем все права на БД shop
-grant all on gb_shop.* to shop;
+-- Добавляем юзера shop с доступом только с локального хоста
+create user 'shop'@'localhost' identified by '1234';
+-- Даем все права на БД gb_shop
+grant all on gb_shop.* to 'shop'@'localhost';
 
-drop user shop;
 
-select * from user;
+/**
+*	Задание 2.
+*	Пусть имеется таблица accounts содержащая три столбца id, name, password,
+*	содержащие первичный ключ, имя пользователя и его пароль.
+*	Создайте представление username таблицы accounts, предоставляющий доступ к столбца id и name.
+*	Создайте пользователя user_read, который бы не имел доступа к таблице accounts,
+*	однако, мог бы извлекать записи из представления username.
+*/
+-- Представление (назову accounts_view) буду добавлять в БД gb_vk, которое будет объединять значения таблиц users и profiles.
 
-flush privileges;
+-- Поэтому выбираю БД gb_vk
+use gb_vk;
+-- Создаю представление
+create or replace view accounts_view as
+	select
+		u.id,
+		p.id as profile_id,
+		p.name,
+		p.surname,
+		u.email,
+		u.phone,
+		p.birthday
+	from users as u
+	left join profiles as p on p.user_id = u.id;
 
-select user();
+-- Проверяю что отдает представление
+select * from accounts_view;
 
-show privileges;
+-- Теперь создаю пользователя user_read в mysql без пароля
+create user 'user_read'@'%';
+-- Даю пользователю право только на чтение в БД gb_vk только представления accounts_view
+grant select on gb_vk.accounts_view to 'user_read'@'%';
+-- Проверяю из консоли...
+-- Пример в скриншотах приложен.
